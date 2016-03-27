ef34309a458892b0239575f717cf6b0639aa459c
lightings <songliang@SLMAC.local>
2016-03-27 21:01:38 +0800

    o  faf

--- hello/src/hello.go
+++ hello/src/hello.go
@@ -1,11 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
-var i t = 6
+var i = 6
+
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
 
 func main() {
-	fmt.Println("Hello, world")
-	math.abs(0)
 
+	go say("world")
+
+	fmt.Println("Hello, world")
+	fmt.Println("hello ", rand.Intn(10))
+
+	for i := 0; i < 8; i++ {
+		time.Sleep(200 * time.Millisecond)
+		fmt.Println("comp")
+	}
 }
